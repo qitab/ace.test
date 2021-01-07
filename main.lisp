7290760ec2ae54cfe9ea09da7428d92b7ececc11
Jonathan Godbout <jgodbout@google.com>
2021-01-07 14:59:37 -0800

    Switch google.core/google.test to ace.core/ace.test
    Namespace re-naming to conform to open source standards.
    
    PiperOrigin-RevId: 350644889

--- main.lisp
+++ main.lisp
@@ -13,7 +13,7 @@
 (ace.core.package:defpackage* :ace.test.main
   (:use :cl)
   (:use-alias #:ace.core.thread
-              #+google3 #:google.flag))
+              #+google3 #:ace.flag))
 
 (in-package :ace.test.main)
 
